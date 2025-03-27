04f5d1d9c0497a8a762923ab6ac2775c7a89068b
HaroldOboe <141307300+HaroldOboe@users.noreply.github.com>
2025-03-27 02:52:12 +0000

    Add instructions

--- app/components/results.tsx
+++ app/components/results.tsx
@@ -26,7 +26,9 @@ export default function IndexPage() {
         </p>
       )}
       {!subject && !subjNum && !duration && (
-        <p className="text-2xl text-gray-500">No form data submitted yet.</p>
+        <p className="text-2xl text-gray-500">
+          No form data submitted yet. Visit /admin to input data.
+        </p>
       )}
     </div>
   );
